72937fb96755b25bf4f2c40ec066289cc8755147
Poype <253186926@qq.com>
2025-05-14 07:36:16 +0800

    Spring Security Architecture Note

---
+++ src/main/java/com/poype/spring_security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.poype.spring_security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.csrf(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+                .authorizeHttpRequests(authorize ->
+                        authorize.anyRequest().authenticated()
+                );
+
+        return httpSecurity.build();
+    }
+}
