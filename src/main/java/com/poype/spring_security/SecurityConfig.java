7d4ae579a7e6f5a66ca144415a25337ebca87d51
Poype <253186926@qq.com>
2025-06-08 17:08:35 +0800

    对默认配置的注释解释 如果不加任何配置，那会有一个默认配置，默认配置跟这里的配置是一样的

--- src/main/java/com/poype/spring_security/SecurityConfig.java
+++ src/main/java/com/poype/spring_security/SecurityConfig.java
@@ -5,36 +5,28 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    // 如果不加任何配置，那会有一个默认配置，默认配置跟这里的配置是一样的
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
 
-        httpSecurity.csrf(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults())
-                .formLogin(Customizer.withDefaults())
-                .authorizeHttpRequests(authorize ->
-                        authorize.anyRequest().authenticated()
-                );
+        // 这行配置会增加BasicAuthenticationFilter，它是浏览器弹窗认证，正常不会被使用到
+//        httpSecurity.httpBasic(Customizer.withDefaults());
+
+        // 这行配置会增加3个filter：UsernamePasswordAuthenticationFilter、DefaultLoginPageGeneratingFilter、DefaultLogoutPageGeneratingFilter
+        httpSecurity.formLogin(Customizer.withDefaults());
+
+        httpSecurity.authorizeHttpRequests((authorize) -> {
+            authorize.anyRequest().authenticated();  // 所有的request都要进行认证，已经被authenticated的用户就能够access
+        });
 
         return httpSecurity.build();
     }
-
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-//
-//        manager.createUser(
-//                User
-//                    .withDefaultPasswordEncoder()
-//                    .username("test")
-//                    .password("abc")
-//                    .roles("USER")
-//                    .build()
-//        );
-//        return manager;
-//    }
 }
